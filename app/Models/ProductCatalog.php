864638dc9b8b01fdb95a5a0d9f36b9b719e92d57
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-02-05 11:56:53 +0700

    feat/product-catalog : add relation belongs to vendor.

--- app/Models/ProductCatalog.php
+++ app/Models/ProductCatalog.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ProductCatalog extends Model
 {
+    use HasFactory;
+
     protected $fillable=[
         'name',
         'description',
         'price',
         'vendor_id'
     ];
+
+    public function vendor():BelongsTo{
+        return $this->belongsTo(Vendor::class, "vendor_id", "id");
+    }
 }
