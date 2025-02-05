f00308c22253e755f71de8cd6990867f9c322597
nullsec45 <rama21010ti@student.nurulfikri.ac.id>
2025-02-05 11:57:08 +0700

    feat/vendor : add relation has many vendor.

--- app/Models/Vendor.php
+++ app/Models/Vendor.php
@@ -3,13 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Vendor extends Model
 {
+    use HasFactory;
+    
     protected $fillable=[
         'name',
         'email',
         'phone',
         'is_approved'
     ];
+
+    public function products():HasMany{
+        return $this->hasMany(ProductCatalog::class,'vendor_id','id');
+    }
 }
